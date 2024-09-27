768a1b12030c7a486270f513f362a1c5fc3ebd2a
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-09-26 22:56:41 -0300

    feat: telas de transição

--- controller/telas.py
+++ controller/telas.py
@@ -1,14 +1,83 @@
 def cabecalho(title: str) -> None:
-    line = "=" * (len(title) + 2)
+    line = "=" * 29
     print(f"=|{line}|=")
-    print(f" | {title} |")
+    print(f" | {title.title():^28}|")
     print(f"=|{line}|=")
+
+
+def opcoes(*args) -> None:
+    for index, option in enumerate(args, start=1):
+        if option == "sair" or option == "menu principal":
+            print(f"=| 0. {option.title():<24} |=")
+        else:
+            print(f"=| {index}. {option.title():<24} |=")
+    print(f"{'=' * 33}")
+
+
+def tela_principal() -> None:
+    cabecalho("hamburgueria py")
+    opcoes(
+        "almoxarifado",
+        "funcionarios",
+        "clientes",
+        "relatórios",
+        "informações",
+        "sair")
+
+
+def tela_almoxarifado() -> None:
+    cabecalho("almoxarifado")
+    opcoes(
+        "adicionar compra",
+        "visualizar almoxarifado",
+        "buscar item",
+        "excluir item",
+        "editar item",
+        "menu principal")
+
+
+def tela_funcionarios() -> None:
+    cabecalho("funcionários")
+    opcoes(
+        "contratar funcionário",
+        "visualizar funcionário",
+        "buscar funcionário",
+        "demitir funcionário",
+        "editar funcionário",
+        "menu principal")
+
+
+def tela_clientes() -> None:
+    cabecalho("clientes")
+    opcoes(
+        "adicionar cliente",
+        "visualizar cliente",
+        "buscar cliente",
+        "excluir cliente",
+        "editar cliente",
+        "menu principal")
+
+
+def tela_relatorios() -> None:
+    cabecalho("relatórios")
+    opcoes(
+        "relatório de compra",
+        "relatorio de vendas",
+    )
+
+
+def tela_informacoes() -> None:
+    cabecalho("informações")
+    opcoes(
+        "Anderson G. Pereira Cruz",
+        "github: andersoncruz-13"
+    )
 
 
 if __name__ == "__main__":
-    cabecalho("TELA PRINCIPAL")
-    cabecalho("ALMOXARIFADO")
-    cabecalho("FUNCIONARIOS")
-    cabecalho("CLIENTES")
-    cabecalho("RELATORIOS")
-    cabecalho("INFORMAÇÕES")
+    tela_principal()
+    tela_almoxarifado()
+    tela_funcionarios()
+    tela_clientes()
+    tela_relatorios()
+    tela_informacoes()
