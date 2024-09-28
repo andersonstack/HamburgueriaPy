92f6dbcf7b1f1e45026ba2c7112ca353a9eeeb93
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-09-28 13:49:22 -0300

    feat: add métodos para validação de compras

--- almoxarifado/controller.py
+++ almoxarifado/controller.py
@@ -1,4 +1,5 @@
 from data.saveJson import SaveJson
+from typing import Dict
 
 
 class Almoxarifado:
@@ -7,14 +8,19 @@ class Almoxarifado:
         self.price = 0.0
         self.quantity = 0
         self.almoxarifado = SaveJson("almoxarifado.json")
-        self.len_almoxarifado = self.almoxarifado.load_json()
+        self.load_almoxarifado = self.almoxarifado.load_json()
 
     def add_buy(self, name: str, quantity: int) -> int:
-        # Gerar um novo ID baseado no maior ID existente
-        if self.len_almoxarifado:
-            id_ = len(self.len_almoxarifado) + 1
+        if self.load_almoxarifado:
+            id_ = len(self.load_almoxarifado) + 1
         else:
-            id_ = 1  # Começa com 1 se não houver itens
+            id_ = 1
+
+        add_buy = self._verify_buy(name)
+        if len(add_buy) > 0:
+            for key in add_buy:
+                self._udpate_quantity(key, quantity)
+                return key
 
         new_item = {id_: [name, quantity]}
         self.almoxarifado.modify_json(data=new_item)
@@ -28,3 +34,14 @@ class Almoxarifado:
 
     def edit_buy(self, id) -> dict:
         return {}
+
+    def _verify_buy(self, name) -> Dict[int, list]:
+        for cod, det in self.load_almoxarifado.items():
+            if name.upper() == det[0].upper():
+                return {cod: det}
+        return {}
+
+    def _udpate_quantity(self, key, quantity):
+        if key in self.load_almoxarifado:
+            self.load_almoxarifado[key][1] += quantity
+            self.almoxarifado.modify_json(self.load_almoxarifado)
