9e9223ea3f5786fcde44e9a7f5b789c423e9c5cb
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-10-04 16:31:13 -0300

    feat: add lógica de adição de um novo funcionário

--- employee/controller.py
+++ employee/controller.py
@@ -1,5 +1,32 @@
-from view.screens import clear
+from view.screens import clear, _basic
 from employee.view import screen_employee
+from controller.inputs import inputInt
+from employee.model import Employee
+from view.styles import printS, printW
+
+
+def new_employee():
+    _basic("Adicionando novo funcionário")
+
+    name = input("Nome:\t")
+    age = inputInt("Idade:\t")
+    cpf = input("CPF:\t")
+    adress = input("Endereço:\t")
+    phone = input("Telefone:\t")
+
+    contract = Employee()
+    if not contract.add_employee(
+                            name=name,
+                            age=age,
+                            cpf=cpf,
+                            adress=adress,
+                            phone=phone
+                    ):
+        printW(f"Não foi possível adicionar {cpf} ao sistema. CPF já existe!")
+        return
+
+    printS(f"{name} foi adicionado ao sistema com sucesso!")
+    input()
 
 
 def main_employee() -> None:
@@ -10,7 +37,7 @@ def main_employee() -> None:
 
         match option:
             case '1':
-                ...
+                new_employee()
             case '2':
                 ...
             case '3':
--- employee/model.py
+++ employee/model.py
@@ -8,13 +8,25 @@ class Employee:
         self.cpf = ""
         self.adress = ""
         self.phone = ""
-        self.employee = SaveJson("funcionrios.json")
+        self.employee = SaveJson("funcionarios.json")
         self.load_employee = self.employee.load_json()
 
     def add_employee(
             self, name: str, cpf: str,
-            age: int, adress: str, phone: str) -> None:
-        ...
+            age: int, adress: str, phone: str) -> bool:
+
+        if self._verify_cpf(cpf):
+            new_employee = {
+                cpf: [
+                    name,
+                    age,
+                    adress,
+                    phone
+                ]
+            }
+            self.employee.modify_json(data=new_employee)
+            return True
+        return False
 
     def remove_employee(self, cpf: str) -> None:
         ...
@@ -24,3 +36,8 @@ class Employee:
 
     def search_employee(self, cpf: str) -> None:
         ...
+
+    def _verify_cpf(self, cpf: str) -> bool:
+        if cpf in self.load_employee:
+            return False
+        return True
