79776fcd007a005038b12983d2badeebb43f4892
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-10-11 09:16:49 -0300

    short: add docstring

--- customer/model.py
+++ customer/model.py
@@ -1,5 +1,12 @@
 class Client:
     def __init__(self):
+        """
+        Constructor method.
+
+        Parameters:
+        self (Client): The instance of the class
+        """
+        self.table = 0
         self.name = ""
         self.adress = ""
 
@@ -7,4 +14,15 @@ class Client:
             self, name: str,
             adress: str | None = None,
             table: int | None = None) -> None:
+        """
+        Creates a new client.
+
+        Parameters:
+        name (str): The name of the client
+        adress (str | None): The adress of the client (optional)
+        table (int | None): The table of the client (optional)
+
+        Returns:
+        None
+        """
         ...
