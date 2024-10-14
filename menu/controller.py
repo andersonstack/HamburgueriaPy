94c3b879216f4f99d5e8a9f8dbff94f0c4014e80
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-10-14 15:56:39 -0300

    feat: add função para visualizar cardápio

--- menu/controller.py
+++ menu/controller.py
@@ -2,7 +2,7 @@ from menu.view import screen_menu
 from menu.model import Menu
 from view.screens import clear, _basic
 from view.styles import printW
-from controller.inputs import inputStr, inputFloat
+from controller.inputs import inputStr, inputFloat, inputInt
 
 
 def add_hamburguer():
@@ -31,6 +31,23 @@ def add_hamburguer():
     hamburguer.close_connection()
 
 
+def view_menu() -> None:
+    menu = Menu()
+    menu.view_menu()
+    menu.close_connection()
+    printW("> Enter para continuar")
+    input()
+
+
+def search_burguer() -> None:
+    cod = inputInt("Código: ")
+    menu = Menu()
+    menu.visualize_hamburguer(cod)
+    menu.close_connection()
+    printW("> Enter para continuar")
+    input()
+
+
 def main_menu() -> None:
     while True:
         clear()
@@ -40,13 +57,13 @@ def main_menu() -> None:
         while True:
             match option:
                 case '1':
-                    # add
+                    add_hamburguer()
                     ...
                 case '2':
-                    # view
+                    view_menu()
                     ...
                 case '3':
-                    # search
+                    search_burguer()
                     ...
                 case '4':
                     # delete
