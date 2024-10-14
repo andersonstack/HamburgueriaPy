89e7265ac8f45a621fc47a03cae446ce981908ec
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-10-14 13:53:28 -0300

    feat: add método de adicionar um hambúrguer ao cardápio

---
+++ menu/model.py
@@ -0,0 +1,41 @@
+from data.saveFiles import SaveData
+from typing import List
+from json import dumps
+import sqlite3
+
+
+class Menu(SaveData):
+    def __init__(self) -> None:
+        super().__init__("./data/cardapio.db")
+        self.conn = sqlite3.connect(self.db_path)
+        self.create_table()
+
+    def create_table(self) -> None:
+        with self.conn:
+            self.conn.execute(
+                """
+                CREATE TABLE IF NOT EXISTS data(
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT UNIQUE,
+                    ingredients LIST,
+                    price FLOATING
+                )
+                """
+            )
+
+    def insert_data(self, name: str, ingredients: List[str],
+                    price: float) -> None:
+        with self.conn:
+            self.conn.execute(
+                """
+                INSERT INTO data(name, ingredients, price)
+                VALUES (?, ?, ?)
+                """, (name, dumps(ingredients, ensure_ascii=False), price)
+            )
+
+if __name__ == "__main__":
+    name = "X-TUDO"
+    ingredients = ['Pão', 'Bacon', 'Bacon', 'Salsicha', 'Ovo']
+    price = 10.99
+    x = Menu()
+    x.insert_data(name=name, ingredients=ingredients, price=price)
